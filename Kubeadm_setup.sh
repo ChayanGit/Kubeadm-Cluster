bdc03b5f3bde690b167a63092205a39bd736b176
ChayanGit <chayan.choudhurypersonal@gmail.com>
2022-04-01 17:10:08 +0530

    first commit

---
+++ Kubeadm_setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+
+# Disable Swapping
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+sudo apt install docker.io -y > /dev/null
+
+if [$? == 0]
+then
+  usermod -aG docker ubuntu
+else
+  echo "Docker installation failed"
+fi
+
+systemctl restart docker
+systemctl enable docker.service
+
+sudo apt-get update && apt-get install -y kubelet kubeadm kubectl
+
+chmod 700 /etc/docker
+touch /etc/docker/daemon.json
+echo "{"exec-opts": ["native.cgroupdriver=systemd"]}" > daemon.json
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl restart kubelet
+
---
+++ master_setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+kubeadm init > /root/master/initlog
+
+if [$? == 0]
+then
+    initMasterPane
+else
+    echo "Master Node not working"
+    echo "restart Kubelet and reset kubeadm"
+    kubeadm reset -y
+
+
+initMasterPane (){
+    mkdir -p $HOME/.kube
+    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    sudo chown $(id -u):$(id -g) $HOME/.kube/config
+        
+}
+kubectl get pods — all-namespaces
+
+if [$? == 0]
+then
+    kubectl apply -f “https://cloud.weave.works/k8s/net?k8s-v ersion=$(kubectl version | base64 | tr -d ‘\n’)”
+else
+    echo "core dns nt started"
+fi
