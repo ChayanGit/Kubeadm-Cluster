a89baba65b6a86fc3ea3362d6cc5f5716bd6b54b
ChayanGit <chayan.choudhurypersonal@gmail.com>
2022-04-02 14:25:52 +0530

    changing the files again

--- master_setup.sh
+++ master_setup.sh
@@ -4,18 +4,10 @@ kubeadm init >> /root/master/initlog
 
 initMasterPane
 
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-initMasterPane (){
-    mkdir -p $HOME/.kube
-    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-    sudo chown $(id -u):$(id -g) $HOME/.kube/config
-        
-}
-kubectl get pods --all-namespaces
+#kubectl get pods --all-namespaces
 
-if [$? == 0]
-then
-    kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-else
-    echo "core dns nt started"
-fi
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
